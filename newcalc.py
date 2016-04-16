e2e23c95e0e5980024a14c22ebc2852c5b928f05
Bhavesh Shah <bhavrish@gmail.com>
2016-04-16 17:13:52 -0400

    Update newcalc.py

--- newcalc.py
+++ newcalc.py
@@ -375,6 +375,27 @@ def cot(angle):
 def cis(angle):
     answer =  math.sin(angle) + "i" + math.cos(angle) + "i"
     return answer
+    
+def rounding(number):
+	if number - math.floor(number) < .5:
+		return math.floor(number)
+	elif number - math.floor(number) >= .5:
+		return math.floor(number) + 1
+	
+
+def degToMinSec(degree):
+	deg = math.floor(degree)
+	min = math.floor((degree - deg) * 60)
+	sec = rounding((((degree - deg) * 60) - min) * 60)
+	return str(int(deg)) + "DEG " + str(int(min)) + "'" + str(int(sec)) + "''"
+
+def minSecToDeg (degree, minute, second):
+	if minute > 60:
+		return 'error: minutes greater than 60'
+	elif second > 60:
+		return 'error: seconds greater than 60' 
+	degree += minute / 60.0 + second / 3600.0
+	return degree
 
 # Geometry
 def volumeofsphere(radius):
